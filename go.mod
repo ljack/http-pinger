523c8bf28c4c75037840eecc5ee5c5441ab5f833
Jarkko Lietolahti <jarkko.lietolahti@gmail.com>
2021-02-07 16:05:55 +0200

    add go modules support, run traceroute when error

---
+++ go.mod
@@ -0,0 +1,5 @@
+module http-pinger
+
+go 1.16
+
+require github.com/aeden/traceroute v0.0.0-20181124220833-147686d9cb0f
--- http-pinger.go
+++ http-pinger.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
+	"github.com/aeden/traceroute"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/smtp"
+	"net/url"
 	"os"
 	"strings"
 	"time"
@@ -14,6 +18,8 @@ import (
 var (
 	c      = make(chan string)
 	Config ConfigJson
+	sourceIp, err = http.Get("https://api.ipify.org")
+	options = traceroute.TracerouteOptions{}
 )
 
 type ConfigJson struct {
@@ -30,6 +36,7 @@ type ConfigJson struct {
 }
 
 type Msg struct {
+	Source    string `json:"source_ip"`
 	Server    string `json:"server_url"`
 	Type      string `json:"message_type"`
 	Date      string `json:"date"`
@@ -40,7 +47,21 @@ type Msg struct {
 	Error     string `json:"error"`
 }
 
+
 func main() {
+
+	var m = flag.Int("m", 20, `Set the max time-to-live (max number of hops) used in outgoing probe packets (default is 64)`)
+	var f = flag.Int("f", traceroute.DEFAULT_FIRST_HOP, `Set the first used time-to-live, e.g. the first hop (default is 1)`)
+	var q = flag.Int("q", 1, `Set the number of probes per "ttl" to nqueries (default is one probe).`)
+
+	flag.Parse()
+	// host := flag.Arg(0)
+	// options := traceroute.TracerouteOptions{}
+	options.SetRetries(*q - 1)
+	options.SetMaxHops(*m + 1)
+	options.SetFirstHop(*f)
+
+
 	ParseConfig()
 	file_data, err := ioutil.ReadFile(Config.UrlsFile)
 	if err != nil {
@@ -54,26 +75,38 @@ func main() {
 		}
 	}
 
+
+	var source = httpGet("https://api.ipify.org/")
+
 	for _, url := range urls {
 		fmt.Println(url)
-		go ping(url)
+		go ping(url,source)
 	}
 
 	for {
 		select {
 		case msg := <-c:
+
 			fmt.Println(msg)
 		}
 	}
 }
 
-func ping(url string) {
+func ping(urlString string, source string) {
 	for {
 		start := time.Now()
-		res, err := http.Get(url)
+
+		url2, urlerr := url.Parse(urlString)
+		if urlerr != nil {
+			panic(urlerr)
+		}
+
+		res, err := http.Get(urlString)
+
 		msg := Msg{}
 		if err != nil {
-			msg.Server = url
+			msg.Source = source
+			msg.Server = urlString
 			msg.Type = "Fatal"
 			msg.Date = time.Now().Format("2006-01-02 15:04 -0700")
 			msg.Lag = Config.Lag
@@ -84,22 +117,29 @@ func ping(url string) {
 			b, _ := json.Marshal(msg)
 			str := string(b)
 			EmailMsg(str)
+			tracerouteForHost(url2.Host)
+
 			c <- str
 		} else {
 			lag := time.Since(start)
-			msg.Server = url
+			msg.Source = source
+			msg.Server = url2.Host
 			msg.Date = time.Now().Format("2006-01-02 15:04 -0700")
 			msg.Lag = Config.Lag
 			msg.Interval = Config.Interval
 			msg.Status = res.StatusCode
 			msg.Responsed = lag.String()
 			msg.Type = "OK" // expected
+
 			if res.StatusCode != 200 {
 				msg.Type = "Warning"
 				msg.Error = "Unexpected http status code!"
 				b, _ := json.Marshal(msg)
 				str := string(b)
 				EmailMsg(str)
+				tracerouteForHost(url2.Host)
+
+
 			}
 			if lag > time.Duration(Config.Lag)*time.Second {
 				msg.Type = "Warning"
@@ -107,6 +147,8 @@ func ping(url string) {
 				b, _ := json.Marshal(msg)
 				str := string(b)
 				EmailMsg(str)
+				tracerouteForHost(url2.Host)
+
 			}
 			b, _ := json.Marshal(msg)
 			str := string(b)
@@ -118,7 +160,7 @@ func ping(url string) {
 }
 
 func EmailMsg(msg string) {
-	sendMail(Config.EmailSubject, msg, Config.FromEmail, Config.ToEmails)
+	// sendMail(Config.EmailSubject, msg, Config.FromEmail, Config.ToEmails)
 }
 
 func sendMail(subject string, message string, from string, to []string) {
@@ -134,6 +176,7 @@ func sendMail(subject string, message string, from string, to []string) {
 		fmt.Println("[Warning] Send Email failed: ", err.Error())
 		return
 	}
+
 	fmt.Println("Sent Email Notification to: ", to)
 }
 
@@ -154,4 +197,67 @@ func ParseConfig() {
 		fmt.Println("Parse config.json failed: ", err.Error())
 		os.Exit(1)
 	}
+}
+
+func printHop(hop traceroute.TracerouteHop) {
+	addr := fmt.Sprintf("%v.%v.%v.%v", hop.Address[0], hop.Address[1], hop.Address[2], hop.Address[3])
+	hostOrAddr := addr
+	if hop.Host != "" {
+		hostOrAddr = hop.Host
+	}
+	if hop.Success {
+		fmt.Printf("%-3d %v (%v)  %v\n", hop.TTL, hostOrAddr, addr, hop.ElapsedTime)
+	} else {
+		fmt.Printf("%-3d *\n", hop.TTL)
+	}
+}
+
+
+func tracerouteForHost(host string) {
+
+
+
+	ipAddr, err := net.ResolveIPAddr("ip", host)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("traceroute to %v (%v), %v hops max, %v byte packets\n", host, ipAddr, options.MaxHops(), options.PacketSize())
+
+	c := make(chan traceroute.TracerouteHop, 0)
+	go func() {
+		for {
+			hop, ok := <-c
+			if !ok {
+				fmt.Println()
+				return
+			}
+			printHop(hop)
+		}
+	}()
+
+	_, err = traceroute.Traceroute(host, &options, c)
+	if err != nil {
+		fmt.Printf("Error: ", err)
+	}
+
+}
+
+
+func httpGet( url string) string {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("%s", err)
+		os.Exit(1)
+	} else {
+		defer response.Body.Close()
+		contents, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			fmt.Printf("%s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s\n", string(contents))
+		return string(contents)
+	}
+	return ""
 }
